67d76a43c812b1cfb13eae071580c28a7f2a1011
CaIagFormal <carlos.tecnico.iagnecz@gmail.o>
2025-02-17 14:20:36 -0300

    Conserto dos parâmetros faltando em M_CotacaoJson

--- moedas/src/main/java/kotacoes/moedas/model/M_CotacaoJson.java
+++ moedas/src/main/java/kotacoes/moedas/model/M_CotacaoJson.java
@@ -6,6 +6,10 @@ public class M_CotacaoJson {
 
     @JsonProperty(value="code")
     private String code;
+    @JsonProperty(value="codein")
+    private String codein;
+    @JsonProperty(value="name")
+    private String name;
     @JsonProperty(value="high")
     private String maxima;
     @JsonProperty(value="low")
@@ -16,8 +20,12 @@ public class M_CotacaoJson {
     private String var_pct;
     @JsonProperty(value="bid")
     private String cotacao;
+    @JsonProperty(value="ask")
+    private String ask;
     @JsonProperty(value="timestamp")
     private String data_cota;
+    @JsonProperty(value="create_date")
+    private String create_date;
 
     public String getCode() {
         return code;
@@ -25,6 +33,22 @@ public class M_CotacaoJson {
 
     public void setCode(String code) {
         this.code = code;
+    }
+
+    public String getCodein() {
+        return codein;
+    }
+
+    public void setCodein(String codein) {
+        this.codein = codein;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getMaxima() {
@@ -67,11 +91,27 @@ public class M_CotacaoJson {
         this.cotacao = cotacao;
     }
 
+    public String getAsk() {
+        return ask;
+    }
+
+    public void setAsk(String ask) {
+        this.ask = ask;
+    }
+
     public String getData_cota() {
         return data_cota;
     }
 
     public void setData_cota(String data_cota) {
         this.data_cota = data_cota;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
     }
 }
