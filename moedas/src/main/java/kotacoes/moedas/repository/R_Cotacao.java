5f8bc122ba55d8ac9210c36f9627d3a52d276ad8
CaIagFormal <carlos.tecnico.iagnecz@gmail.o>
2025-02-17 15:24:53 -0300

    Adicionado método getCotacaoByMoeda em R_Cotacao

--- moedas/src/main/java/kotacoes/moedas/repository/R_Cotacao.java
+++ moedas/src/main/java/kotacoes/moedas/repository/R_Cotacao.java
@@ -2,8 +2,16 @@ package kotacoes.moedas.repository;
 
 import kotacoes.moedas.model.M_Cotacao;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface R_Cotacao extends JpaRepository<M_Cotacao, Long> {
+    @Query(value = "select * from cotacao " +
+                    "where code = :moeda"
+            ,nativeQuery = true)
+    List<M_Cotacao> getCotacaoByMoeda(@Param("moeda") String moeda);
 }
